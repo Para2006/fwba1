e7d24b1734316338ba82db8cc2bb30b64c48f1c8
Para2006 <103443216+Para2006@users.noreply.github.com>
2023-06-11 20:17:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(360,360);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('filterimg.png');
+}
